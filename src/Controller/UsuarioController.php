df17795f3612328c60cd87087594e934ea33ae18
R3B4T0 <david.rebato14@gmail.com>
2021-02-08 19:02:12 +0100

    segundo commit dia 08-02-2021

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -4,17 +4,49 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\Usuario;
+use App\Form\Type\RegistrarType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UsuarioController extends AbstractController
 {
     /**
-     * @Route("/usuario", name="usuario")
+     * @Route("/registrar", name="registrar")
      */
-    public function index(): Response
+    public function registrar(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
-        return $this->render('usuario/index.html.twig', [
-            'controller_name' => 'UsuarioController',
+        $usuario = new Usuario();
+        $form = $this->createForm(UsuarioType::class, $usuario)
+                ->add('email', TextType::class)
+                ->add('password', PasswordType::class)
+                ->add('nombre', TextType::class)
+                ->add('apellidos', TextType::class)
+                ->add('telefono', TextType::class)
+                ->add('registrar', SubmitType::class, ['label' => 'Registrar']);
+                
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $usuario = $form->getData();
+
+            //Codificamos el password
+            $usuario->setPassword($encoder->encodePassword($usuario, $usuario->getPassword()));
+            
+            //Guardamos el nuevo artículo en la base de datos
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($usuario);
+            $em->flush();
+
+            return $this->redirectToRoute('inicio');
+        }
+        
+        return $this->render('usuario/registrar.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
